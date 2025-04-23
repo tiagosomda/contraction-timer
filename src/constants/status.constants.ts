7a271c8683225b4817c0a15fb0fee08b7e168d32
Tiago <tsomda@gmail.com>
2025-04-22 22:35:09 -0500

    changed string from Rest to No Contraction

--- src/constants/status.constants.ts
+++ src/constants/status.constants.ts
@@ -3,5 +3,5 @@ import { Status } from '../models/status.model';
 export const STATUS_TO_LABEL_MAP: Record<Status, string> = {
   [Status.Contraction]: 'Contraction',
   [Status.Ready]: 'Ready',
-  [Status.Rest]: 'Rest',
+  [Status.Rest]: 'No Contraction',
 };
