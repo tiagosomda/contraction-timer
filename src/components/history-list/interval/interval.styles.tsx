0a131f0540bfdd2f90121d131076a17dc1fe210f
Tiago <tsomda@gmail.com>
2025-04-22 22:34:50 -0500

    reverted color changes

--- src/components/history-list/interval/interval.styles.tsx
+++ src/components/history-list/interval/interval.styles.tsx
@@ -14,6 +14,5 @@ export const StyledInterval = styled.li`
     bottom: 0;
     left: ${pxToRem(8)};
     border-left: ${borderWidth('l')} solid ${color('neutralMin')};
-    border-left-color: ${color('rest')}; // Updated to match Rest color
   }
 `;
